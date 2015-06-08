18a7bf5e44285f30fe309ba036640f700810c3eb
Summermicrosystemsut <summermicrosystemsut@gmail.com>
2015-06-07 22:15:17 -0400

    rewrote c++ program in IAR, rewrote assembly program in IAR, copied main portion of disassembly to text file

---
+++ IntroProj/RNG.h
@@ -0,0 +1,4 @@
+#pragma once
+
+	EXTERN	RNG_Init
+	EXTERN	RNG_Get_Number
--- Project #0 Assembly/asm.s
+++ Project #0 Assembly/asm.s
@@ -16,9 +16,20 @@ __iar_program_start
         SECTION .text : CODE (2)
 	THUMB
 
-main
-
-	B	main
-
-
+	MOV	R0, #0				;INITIALIZE FIRST FIBB NUM
+	MOV	R1, #1				;INITIALIZE SECOND FIB NUM
+	MOV	R3, #25				;iNITIALIZE COUNTER TO 25
+main    
+	PUSH	{R0}
+	
+	ADD	R0, R1				;ADD TWO VALUES OF FIB NUM
+	
+	MOV	R2, R0				;SWAP R0 AND R1
+	MOV	R0, R1
+	MOV	R1, R2
+	
+	SUB	R3, R3, #1			;DECREMENT COUNT
+	CMP	R3, #0				;CHECK IF COUNT IS ZERO
+	BNE	main				;IF ZERO, RESTART
+	
         END
